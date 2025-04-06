a402a779d286521143875e9f4db951455ba41a18
Mahikshith <mahikshith97@gmail.com>
2025-04-06 17:08:51 +0530

    ollama rag
    ollama rag version worked fine

--- ollama_rag.py
+++ ollama_rag.py
@@ -1,49 +1,67 @@
-# 1. reading the pdf using PyMuPDF
-# 2. splitting the text into chunks using langchain text splitter 
-# 3. embedding the text using langchain embedding model
-# 4, storing the embeddings in a vector store using langchain vector store
-# 5. using langchain retriever to retrieve the relevant chunks from the vector store 
-# 6. using ollama to generate the answer using the retrieved chunks as context  
-
-# step 1 : 
-
-#from langchain.document_loaders import PyMuPDFLoader  -- depricated 
-
 from langchain_community.document_loaders import PyMuPDFLoader
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter 
-
-
-# from langchain.embeddings import OllamaEmbeddings  -- depricated
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 
-# from langchain.vectorstores import FAISS  -- depricated 
-
 from langchain_community.vectorstores import FAISS
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_ollama import ChatOllama
 
-MODEL = "llama3.2" 
+from langchain.schema.runnable import RunnablePassthrough  
+from langchain.retrievers.multi_query import MultiQueryRetriever  
 
-file_path =r"C:\Users\mahik\Documents\Git_serious\langchain_langgraph_agents\langgraph_presentation.pdf" 
-
+MODEL = "llama3.2"
 file_path = "./langgraph_presentation.pdf"
 
-# load the pdf file using PyMuPDFLoader
+# Load the PDF file
 loader = PyMuPDFLoader(file_path)
 documents = loader.load()
 
-# print(f"Number of documents loaded: {len(documents)}") 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
 
-# print(documents[3].page_content)
-
-# split the text into chunks using langchain text splitter
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20) 
 texts = text_splitter.split_documents(documents)
 
-# ollama embedding model using faiss vector store 
 embed_model = "nomic-embed-text"
 
 embedding_model = OllamaEmbeddings(model=embed_model)
 vector_store = FAISS.from_documents(texts, embedding_model)
-retriever = vector_store.as_retriever(search_kwargs={"k": 2})
 
+# Define the LLM
+llm = ChatOllama(model=MODEL)
+
+# Prompt for multi-query retriever
+query_prompt = PromptTemplate(
+
+    template="""You are a helpful assistant. Your task is to generate five 
+    different versions of the given user question to retrieve relevant documents from
+    a vector database. By generating multiple perspectives on the user question, your
+    goal is to help the user overcome some of the limitations of the distance-based
+    similarity search. Provide these alternative questions separated by newlines.
+    Original question: {question}""",
+
+    input_variables=["question"],
+)
+
+retriever = MultiQueryRetriever.from_llm(
+    vector_store.as_retriever(),
+    prompt=query_prompt,
+    llm=llm,
+)
+
+# RAG  template
+
+
+template = """You are a helpful assistant. You will be provided with a question and some context, {context}.
+Your task is to answer the question, {question} based on the context provided. \
+If the context does not contain enough information to answer the question, \
+say "I couldn't find that information in the provided document."""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}  
+    | prompt
+    | llm  
+)
+
+ans = chain.invoke(input="What is langgraph?")
+print(ans.content) 
